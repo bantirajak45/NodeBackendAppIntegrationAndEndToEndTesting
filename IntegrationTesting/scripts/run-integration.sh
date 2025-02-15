573359bc3e667da58c3fc19f56d7da2714be1f29
bantirajak45 <99631424+bantirajak45@users.noreply.github.com>
2025-02-15 22:08:09 +0530

    Update run-integration.sh
    add dependency of vitest cause i ignore node_module folder to github

--- IntegrationTesting/scripts/run-integration.sh
+++ IntegrationTesting/scripts/run-integration.sh
@@ -3,5 +3,6 @@ echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
 echo '🟡 - database is ready...'
 npx prisma migrate dev --name init
+npm install vitest
 npm run test
 docker-compose down
