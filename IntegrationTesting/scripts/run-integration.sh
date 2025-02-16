99068c5119614773964ccc15038a66e196a1f2df
bantirajak45 <99631424+bantirajak45@users.noreply.github.com>
2025-02-16 13:02:53 +0530

    Update run-integration.sh
    change docker command

--- IntegrationTesting/scripts/run-integration.sh
+++ IntegrationTesting/scripts/run-integration.sh
@@ -1,8 +1,8 @@
-docker-compose up -d
+docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
 echo '🟡 - database is ready...'
 npx prisma migrate dev --name init
 npm install vitest
 npm run test
-docker-compose down
+docker compose down
